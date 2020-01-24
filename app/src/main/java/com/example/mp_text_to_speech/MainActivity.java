0e0f0f649d0f17f1f91306e33d453b680c510154
Yash Gosalpuria <54258460+Yash-404@users.noreply.github.com>
2020-01-25 00:22:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mp_text_to_speech/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.mp_text_to_speech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnspeak;
+    EditText edtspeak;
+
+    TextToSpeech textToSpeech;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = textToSpeech.setLanguage(Locale.ENGLISH);
+                    if (result == TextToSpeech.LANG_MISSING_DATA ||
+                            result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Toast.makeText(MainActivity.this, "This language is not supported", Toast.LENGTH_SHORT).show();
+                    } else {
+                        btnspeak.setEnabled(true);
+                        textToSpeech.setPitch(0.6f);
+                        textToSpeech.setSpeechRate(1.0f);
+                        speak();
+                    }
+                }
+            }
+        });
+
+        edtspeak = (EditText) findViewById(R.id.edt_speak);
+        btnspeak = (Button) findViewById(R.id.btn_speak);
+        btnspeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speak();
+            }
+        });
+    }
+
+    private void speak() {
+        String text = edtspeak.getText().toString();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        } else {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(textToSpeech != null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
+    }
+}
